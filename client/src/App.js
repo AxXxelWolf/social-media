480a328110be1204fca6e9b9be984c30d728a979
Akshy <akshysakthimades@gmail.com>
2023-05-06 01:00:46 +0530

    user widget

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ import { themeSettings } from "./theme";
 function App() {
   const mode = useSelector((state) => state.mode); //grab current state
   const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]); //By wrapping the createTheme function in useMemo, we ensure that it is only called when the mode value changes.
-
+  const isAuth = useSelector((state) => state.token);
   return (
     <div className="app">
       <BrowserRouter>
@@ -19,8 +19,14 @@ function App() {
           <CssBaseline />
           <Routes>
             <Route path="/" element={<LoginPage />} />
-            <Route path="/home" element={<HomePage />} />
-            <Route path="/profile/:userId" element={<ProfilePage />} />
+            <Route
+              path="/home"
+              element={isAuth ? <HomePage /> : <Navigate to="/" />}
+            />
+            <Route
+              path="/profile/:userId"
+              element={isAuth ? <ProfilePage /> : <Navigate to="/" />}
+            />
           </Routes>
         </ThemeProvider>
       </BrowserRouter>
--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -2,7 +2,7 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import User from "../models/User.js";
 
-/*REGISTER USER*/
+/* REGISTER USER */
 export const register = async (req, res) => {
   try {
     const {
@@ -15,19 +15,21 @@ export const register = async (req, res) => {
       location,
       occupation,
     } = req.body;
+
     const salt = await bcrypt.genSalt();
     const passwordHash = await bcrypt.hash(password, salt);
+
     const newUser = new User({
       firstName,
       lastName,
       email,
-      password,
+      password: passwordHash,
       picturePath,
       friends,
       location,
       occupation,
-      viewedProfile: Math.floor(Math.random * 10000),
-      impressions: Math.floor(Math.random * 10000),
+      viewedProfile: Math.floor(Math.random() * 10000),
+      impressions: Math.floor(Math.random() * 10000),
     });
     const savedUser = await newUser.save();
     res.status(201).json(savedUser);
@@ -36,17 +38,17 @@ export const register = async (req, res) => {
   }
 };
 
-/*LOGIN USER*/
+/* LOGGING IN */
 export const login = async (req, res) => {
   try {
     const { email, password } = req.body;
     const user = await User.findOne({ email: email });
-    if (!user) return res.status(400).json({ msg: "User does not exist." });
+    if (!user) return res.status(400).json({ msg: "User does not exist. " });
 
     const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch) return res.status(400).json({ msg: "Invalid credentials" });
+    if (!isMatch) return res.status(400).json({ msg: "Invalid credentials. " });
 
-    const token = jwt.sign({ id: user_id }, process.env.JWT_SECRET);
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
     delete user.password;
     res.status(200).json({ token, user });
   } catch (err) {
